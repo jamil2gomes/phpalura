f83680098409fbfc03189188b5edaa60ee1c984c
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-22 16:43:58 -0300

    resolucao do capitulo 5

---
+++ capitulo5/tarefa2.php
@@ -0,0 +1,40 @@
+<?php
+    session_start(); 
+
+    if(isset($_GET['nome']) && $_GET['nome'] != ''){
+        $tarefa = array();
+        $tarefa['nome'] = $_GET['nome'];
+
+        if(isset($_GET['descricao'])){
+            $tarefa['descricao'] = $_GET['descricao'];
+        }else{
+            $tarefa['descricao'] = '';
+        }
+
+        if(isset($_GET['prazo'])){
+            $tarefa['prazo'] = $_GET['prazo'];
+        }else{
+            $tarefa['prazo'] = '';
+        }
+
+        if(isset($_GET['prioridade'])){
+            $tarefa['prioridade'] = $_GET['prioridade'];
+        }else{
+            $tarefa['prioridade'] = '';
+        }
+
+        if(isset($_GET['concluida'])){
+            $tarefa['concluida'] = $_GET['concluida'];
+        }else{
+            $tarefa['concluida'] = 'não';
+        }
+
+        $_SESSION['lista'][] = $tarefa;
+    }
+    
+    if(isset($_SESSION['lista'])){
+        $lista_tarefas =  $_SESSION['lista'];
+    }else{
+        $lista_tarefas = array();
+    }
+
---
+++ capitulo5/template2.php
@@ -0,0 +1,101 @@
+<?php include 'tarefa2.php'?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gerenciador de tarefas</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class ="container">
+    <h1>Gerenciador de tarefas</h1>
+
+    <form>
+  <div class="form-group">
+      <label for="nome">Nome</label>
+      <input type="text" class="form-control is-valid"  name="nome"  required>
+    </div>
+
+    <div class="form-row">
+   
+      <label for="prazo">Prazo</label>
+      <input type="text" class="form-control is-invalid"   name ="prazo"  >
+    </div>
+ 
+    <div class="form-group">
+    <label for="descricao">Descrição</label>
+    <textarea class="form-control is-invalid" id="validationTextarea" name="descricao"  ></textarea>
+    </div>
+
+
+
+  <fieldset class="form-group">
+    <div class="row">
+      <legend >Prioridade</legend>
+      <div class="col-sm-10">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="prioridade" id="gridRadios1" value="baixa" checked>
+          <label class="form-check-label" for="gridRadios1">
+            Baixa
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="prioridade" id="gridRadios2" value="media">
+          <label class="form-check-label" for="gridRadios2">
+            Média
+          </label>
+        </div>
+        <div class="form-check ">
+          <input class="form-check-input" type="radio" name="prioridade" id="gridRadios3" value="alta" >
+          <label class="form-check-label" for="gridRadios3">
+            Alta
+          </label>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+
+  <div class="form-group">
+    <div class="form-check">
+      Concluída: <input  type="checkbox" value="sim" name="concluida" >
+    </div>
+  </div>
+  <input type="submit" class="btn btn-success btn-block" value="Salvar">
+</form>
+    <br><br>
+    <hr>
+
+ <div class="row">
+    <div class="col-md-12">
+    <table class="table table-hover">
+    <thead >
+        <tr>
+            <th class="col">Tarefa</th>
+            <th class="col">Descrição</th>
+            <th class="col">Prazo</th>
+            <th class="col">Prioridade</th>
+            <th class="col">Concluída</th>
+        </tr>
+    </thead>
+
+       <?php if(isset($lista_tarefas) && is_array($lista_tarefas) && sizeof($lista_tarefas) > 0) : ?>
+            <?php foreach($lista_tarefas as $tarefa) : ?>
+            
+                <tr>
+                    <td><?php echo isset($tarefa['nome']) ? $tarefa['nome'] : ''; ?></td>
+                    <td><?php echo isset($tarefa['descricao']) ? $tarefa['descricao'] : ''; ?></td>
+                    <td><?php echo isset($tarefa['prazo']) ? $tarefa['prazo'] : ''; ?></td>
+                    <td><?php echo isset($tarefa['prioridade']) ? $tarefa['prioridade'] : ''; ?></td>
+                    <td><?php echo isset($tarefa['concluida']) ? $tarefa['concluida'] : ''; ?></td>
+                </tr>
+
+            <?php endforeach; ?>
+        <?php endif;?>
+    </table>
+</div>
+</div>
+</div>
+</body>
+</html>
