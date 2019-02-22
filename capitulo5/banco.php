96ecb9ecbd3758b21880c161ec342b83b3145ac1
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-22 20:11:11 -0300

    insercao do arquivo banco.php que faz conexao com o mysql

---
+++ capitulo5/banco.php
@@ -0,0 +1,37 @@
+<?php
+
+$bdServidor = '127.0.0.1';
+$bdUsuario = 'root';
+$bdSenha = 'j4m1l!123';
+$bdBanco = 'tarefas';
+
+$conexao = mysqli_connect($bdServidor,$bdUsuario,$bdSenha,$bdBanco);
+
+if(mysqli_connect_errno($conexao)){
+    echo "Problema ao conectar no banco. Erro: ";
+    echo mysqli_connect_error();
+    die();
+}
+
+function buscar_tarefas($conexao){
+    $sql = "SELECT * FROM tarefa";
+
+    $resultado = mysqli_query($conexao, $sql);
+
+    $tarefas =[];
+    while ($tarefa = mysqli_fetch_assoc($resultado)) {
+        $tarefas[]=$tarefa;
+    }
+    return $tarefas;
+}
+
+function gravar_tarefa($conexao,$tarefa){
+    $sql = "INSERT INTO tarefa (nome, descricao, prioridade)
+    VALUES (
+                '{$tarefa['nome']}',
+                '{$tarefa['descricao']}',
+                {$tarefa['prioridade']}
+            )";
+
+    mysqli_query($conexao,$sql);
+}
