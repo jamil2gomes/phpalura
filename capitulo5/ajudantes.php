9219c8bc2068a8a7d5eb8f9997a5259c49516a18
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-22 21:08:26 -0300

    criacao dos metodos de formatar data para o banco e a data para ser exibida

--- capitulo5/ajudantes.php
+++ capitulo5/ajudantes.php
@@ -16,3 +16,32 @@ function traduz_prioridade($codigo){
     }
     return $prioridade;
 }
+
+function traduz_data_para_banco($data){
+    if($data==""){
+        return "";
+    }
+
+    $dados = explode("/",$data);
+    $data_banco = "{$dados[2]}-{$dados[1]}-{$dados[0]}";
+    return $data_banco;
+}
+
+function traduz_data_para_exibir($data){
+    if($data == "" || $data =="0000-00-00"){
+        return "";
+    }
+
+    $dados = explode("-", $data);
+    $data_exibir = "{$dados[2]}/{$dados[1]}/{$dados[0]}";
+    return $data_exibir;
+}
+
+function traduz_data_para_exibir_versao2($data){
+    if($data == "" || $data =="0000-00-00"){
+        return "";
+    }
+
+    $objeto_data = DateTime::createFromFormat('Y-m-d', $data);
+    return $objeto_data->format('d/m/Y');
+}
