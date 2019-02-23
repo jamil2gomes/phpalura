3077c2029c8b9099b93d9caee03776c9e0a937e3
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-23 11:14:12 -0300

    atualização da estrutura condicional if para operador ternario, criação da variavel responsavel por exibir ou não a tabela e criação da variavel que seta valores padroes no formulário

--- capitulo5/tarefa2.php
+++ capitulo5/tarefa2.php
@@ -3,36 +3,34 @@
     require_once 'banco.php';
     require_once 'ajudantes.php';
 
+    $exibir_tabela = true;
+
     if(isset($_GET['nome']) && $_GET['nome'] != ''){
-        $tarefa = array();
-        $tarefa['nome'] = $_GET['nome'];
+        $tarefa = [];
+        $tarefa['nome'] =  $_GET['nome'];
 
-        if(isset($_GET['descricao'])){
-            $tarefa['descricao'] = $_GET['descricao'];
-        }else{
-            $tarefa['descricao'] = '';
-        }
-
-        if(isset($_GET['prazo'])){
-            $tarefa['prazo'] =traduz_data_para_banco( $_GET['prazo']);;
-        }else{
-            $tarefa['prazo'] = '';
-        }
-
-        if(isset($_GET['prioridade'])){
-            $tarefa['prioridade'] = $_GET['prioridade'];
-        }else{
-            $tarefa['prioridade'] = '';
-        }
-
-        if(isset($_GET['concluida'])){
-            $tarefa['concluida'] = 1;
-        }else{
-            $tarefa['concluida'] = 0;
-        }
-
+        $tarefa['descricao'] = (isset($_GET['descricao']))? $_GET['descricao']: '';
+        
+        $tarefa['prazo'] = (isset($_GET['prazo'])) ? traduz_data_para_banco( $_GET['prazo']):'';
+      
+        $tarefa['prioridade'] = isset($_GET['prioridade']) ? $_GET['prioridade'] : '';
+        
+        $tarefa['concluida'] = (isset($_GET['concluida'])) ? 1 : 0;
+       
        gravar_tarefa($conexao,$tarefa);
+       header('Location: tarefa2.php');
+       die();
     }
     
    $lista_tarefas = buscar_tarefas($conexao);
+   
+   $tarefa = [
+       'id'=> 0,
+       'nome'=> '',
+       'descricao'  => '',
+       'prazo'      => '',
+       'prioridade' => 1,
+       'concluida'  => ''];
+
+    require_once 'template2.php';
 
