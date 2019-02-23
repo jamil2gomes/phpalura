716b7f57090addc2a3b46294fb0378ae8876ef4c
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-23 11:15:22 -0300

    separação do fomulário e da tabela em arquivos separados

--- capitulo5/template2.php
+++ capitulo5/template2.php
@@ -1,102 +1,23 @@
-<?php require_once 'tarefa2.php';?>
-     
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Gerenciador de tarefas</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <title>Gerenciador de tarefas</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
+  <body>
     <div class ="container">
-    <h1>Gerenciador de tarefas</h1>
-
-    <form>
-  <div class="form-group">
-      <label for="nome">Nome</label>
-      <input type="text" class="form-control is-valid"  name="nome"  required>
+      <h1>Gerenciador de tarefas</h1>
+      <hr>
+      <!-- Exibe Formulário -->
+      <?php require_once'formulario.php'; ?>
+      <br><br>
+      <!-- Exibe Tabela -->
+      <?php if ($exibir_tabela) : ?>            
+        <?php require'tabela.php'; ?>
+      <?php endif; ?>
+      
     </div>
-
-    <div class="form-row">
-   
-      <label for="prazo">Prazo</label>
-      <input type="text" class="form-control is-invalid"   name ="prazo"  >
-    </div>
- 
-    <div class="form-group">
-    <label for="descricao">Descrição</label>
-    <textarea class="form-control is-invalid" id="validationTextarea" name="descricao"  ></textarea>
-    </div>
-
-
-
-  <fieldset class="form-group">
-    <div class="row">
-      <legend >Prioridade</legend>
-      <div class="col-sm-10">
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="prioridade" id="gridRadios1" value="1" checked>
-          <label class="form-check-label" for="gridRadios1">
-            Baixa
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="prioridade" id="gridRadios2" value="2">
-          <label class="form-check-label" for="gridRadios2">
-            Média
-          </label>
-        </div>
-        <div class="form-check ">
-          <input class="form-check-input" type="radio" name="prioridade" id="gridRadios3" value="3" >
-          <label class="form-check-label" for="gridRadios3">
-            Alta
-          </label>
-        </div>
-      </div>
-    </div>
-  </fieldset>
-
-  <div class="form-group">
-    <div class="form-check">
-      Concluída: <input  type="checkbox" value="1" name="concluida" >
-    </div>
-  </div>
-  <input type="submit" class="btn btn-success btn-block" value="Salvar">
-</form>
-    <br><br>
-    <hr>
-
- <div class="row">
-    <div class="col-md-12">
-    <table class="table table-hover">
-    <thead >
-        <tr>
-            <th class="col">Tarefa</th>
-            <th class="col">Descrição</th>
-            <th class="col">Prazo</th>
-            <th class="col">Prioridade</th>
-            <th class="col">Concluída</th>
-        </tr>
-    </thead>
-
-       <?php if(isset($lista_tarefas) && is_array($lista_tarefas) && sizeof($lista_tarefas) > 0) : ?>
-            <?php foreach($lista_tarefas as $tarefa) : ?>
-            
-                <tr>
-                    <td><?php echo isset($tarefa['nome']) ? $tarefa['nome'] : ''; ?></td>
-                    <td><?php echo isset($tarefa['descricao']) ? $tarefa['descricao'] : ''; ?></td>
-                    <td><?php echo traduz_data_para_exibir($tarefa['prazo']); ?></td>
-                    <td><?php echo traduz_prioridade($tarefa['prioridade']); ?></td>
-                    <td><?php echo traduz_concluida($tarefa['concluida']); ?></td>
-                </tr>
-
-            <?php endforeach; ?>
-        <?php endif;?>
-    </table>
-</div>
-</div>
-</div>
-</body>
+  </body>
 </html>
