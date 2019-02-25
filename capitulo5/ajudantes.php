1af7c08dcfd76f579e8d05faa7ce93bbab5d0754
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-25 16:17:52 -0300

    implementaçao da função que trata o arquivo e o salva em um diretório do servidor

--- capitulo5/ajudantes.php
+++ capitulo5/ajudantes.php
@@ -1,5 +1,14 @@
 <?php
 
+function tem_post(){
+    if (count($_POST) > 0){
+        return true;
+    }
+return false;
+}
+
+
+//1.0 - Conversão dos campos prioridade e concluída de número para texto.
 function traduz_prioridade($codigo){
     $prioridade = '';
 
@@ -17,51 +26,19 @@ function traduz_prioridade($codigo){
     return $prioridade;
 }
 
-function traduz_data_para_banco($data){
-    if($data==""){
-        return "";
-    }
-
-    $dados = explode("/",$data);
-
-    if (count($dados) != 3) {
-        return $data;
-    }
-    $objeto_data = DateTime::createFromFormat('d/m/Y', $data);
-    return$objeto_data->format('Y-m-d');
-}
-
-function traduz_data_para_exibir($data){
-    if($data == "" || $data =="0000-00-00"){
-        return "";
-    }
-
-    $dados = explode("-", $data);
-
-    if (count($dados) != 3) {
-        return $data;
-    }
-
-    $data_exibir = "{$dados[2]}/{$dados[1]}/{$dados[0]}";
-    return $data_exibir;
-}
-
-
-
 function traduz_concluida($concluida){
     if ($concluida == 1) {
         return'Sim';        
     }
     return'Não';
 }
+// ------------------------FIM 1.0-----------------------------------
 
-function tem_post(){
-    if (count($_POST) > 0){
-        return true;
-    }
-return false;
-}
 
+
+
+
+// 2.0 - Funções relacionadas as datas
 function validar_data($data){
 
     $padrao = '/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/';
@@ -81,3 +58,52 @@ function validar_data($data){
 
     return $resultado;
 }
+
+
+function traduz_data_para_banco($data){
+    if($data==""){
+        return "";
+    }
+
+    $dados = explode("/",$data);
+
+    if (count($dados) != 3) {
+        return $data;
+    }
+    $objeto_data = DateTime::createFromFormat('d/m/Y', $data);
+    return$objeto_data->format('Y-m-d');
+}
+
+
+function traduz_data_para_exibir($data){
+    if($data == "" || $data =="0000-00-00"){
+        return "";
+    }
+
+    $dados = explode("-", $data);
+
+    if (count($dados) != 3) {
+        return $data;
+    }
+
+    $data_exibir = "{$dados[2]}/{$dados[1]}/{$dados[0]}";
+    return $data_exibir;
+}
+//---------------------------FIM 2.0---------------------------------------
+
+
+//3.0 - Tratamento de arquivos
+
+function tratar_anexo($anexo){
+    $padrao = '/^.+(\.pdf|\.zip)$/';
+    $resultado = preg_match($padrao, $anexo['name']);
+
+    if ($resultado == 0) {
+        return false;
+    }
+
+    move_uploaded_file($anexo['tmp_name'],"anexos/{$anexo['name']}");
+    return true;
+}
+
+//---------------------------FIM 3.0--------------------------------------
